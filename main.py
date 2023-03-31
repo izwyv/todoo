72dc30d33504f0a0507bba1a0a0e6ec2d25945ed
izwyv <125533439+izwyv@users.noreply.github.com>
2023-03-31 03:11:02 +0000

    html and python

--- main.py
+++ main.py
@@ -14,12 +14,27 @@ def index():
         elif action == 'delete':
             task_index = int(request.form.get('task_index'))
             tasks.pop(task_index)
-  
+        elif action == 'check_all':
+            for i in range(len(tasks)):
+                if not tasks[i].endswith('✓'):
+                    tasks[i] += '✓'
+        elif action == "delete_checked":
+            checked_indices = request.form.getlist('task_index')
+            checked_indices = [int(i) for i in checked_indices]
+            checked_indices.sort(reverse=True)
+            for i in checked_indices:
+                if tasks[i].endswith('✓'):
+                    tasks.pop(i)
+                else:
+                    del tasks[i]
+
     # Create a list of tuples where the first element is the task and the second element is its checked status
     task_status_pairs = [(task, task.endswith('✓')) for task in tasks]
-  
+
     return render_template('index.html', task_status_pairs=task_status_pairs)
+  
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0', port=81)
+
 
